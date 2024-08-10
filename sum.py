4310bbe6ef7eb71857612682a1580c8ef2d8bcf9
Saridha V <saridhav17@gmail.com>
2024-08-10 20:50:41 +0530

    added sums.py

---
+++ sum.py
@@ -0,0 +1,8 @@
+arr=list(map(int,input().split()))
+for i in range(len(arr)):
+    mul=arr[i]*arr[i]
+    print("square of the array of :",mul)
+sums=(sum(arr))
+print("sum of the array is:",sums) 
+
+
